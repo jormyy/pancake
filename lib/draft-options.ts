e65c40919123198af79621d954dfdf8dbad69e86
mchen04 <michaelluochen1@gmail.com>
2026-07-09 18:44:42 -0700

    criticality cycle 9: remove type-only draft value

--- lib/draft-options.ts
+++ lib/draft-options.ts
@@ -1,7 +1,5 @@
-const ROOKIE_ROUND_OPTIONS = [2, 3] as const
-
 export type DraftTimerOption = number
-export type RookieRoundOption = (typeof ROOKIE_ROUND_OPTIONS)[number]
+export type RookieRoundOption = 2 | 3
 export const DRAFT_TIMER_MIN_SECONDS = 5
 export const DRAFT_TIMER_MAX_SECONDS = 3600
 
